5d73e8c2b90ec4052e5f1ff251cc10b15ad4340d
Evgeny Safronov <division494@gmail.com>
2018-02-23 01:33:09 +0300

    wip: convert secret

--- src/account.rs
+++ src/account.rs
@@ -1,6 +1,4 @@
-use std::fs::File;
 use std::io::{self, Read};
-use std::path::Path;
 
 use crypto::aessafe::AesSafe128Encryptor;
 use crypto::blockmodes::CtrMode;
@@ -10,6 +8,7 @@ use crypto::symmetriccipher::Decryptor;
 use hex::{FromHex, FromHexError};
 use json;
 use keccak::Keccak;
+use secp256k1::{self, Secp256k1, SecretKey};
 
 #[derive(Debug)]
 pub enum Error {
@@ -18,6 +17,8 @@ pub enum Error {
     FromHexError(FromHexError),
     InvalidCipher(String),
     InvalidPassphrase,
+    InvalidVersion(i32),
+    Secp256k1(secp256k1::Error),
 }
 
 impl From<io::Error> for Error {
@@ -35,6 +36,12 @@ impl From<json::Error> for Error {
 impl From<FromHexError> for Error {
     fn from(err: FromHexError) -> Self {
         Error::FromHexError(err)
+    }
+}
+
+impl From<secp256k1::Error> for Error {
+    fn from(err: secp256k1::Error) -> Self {
+        Error::Secp256k1(err)
     }
 }
 
@@ -78,25 +85,27 @@ pub struct Account {
 }
 
 impl Account {
-    pub fn from_rd<R: Read>(rd: R, password: &str) -> Result<Self, Error> {
-        let account: Account = json::from_reader(rd)?;
-
-        match account.version {
-            3 => decrypt_v3(&account.crypto, password)?,
-            _ => unimplemented!(),
-        };
-
+    pub fn from_rd<R: Read>(rd: R) -> Result<Self, Error> {
+        let account = json::from_reader(rd)?;
         Ok(account)
-    }
-
-    pub fn load<P: AsRef<Path>>(path: P, password: &str) -> Result<Self, Error> {
-        let rd = File::open(path)?;
-        Self::from_rd(rd, password)
     }
 
     #[inline]
     pub fn id(&self) -> &str {
         &self.id
+    }
+
+    pub fn decrypt(&self, password: &str) -> Result<SecretKey, Error> {
+        match self.version {
+            3 => {
+                let key = decrypt_v3(&self.crypto, password)?;
+                let secp = Secp256k1::new();
+                let secret = SecretKey::from_slice(&secp, &key)?;
+
+                Ok(secret)
+            }
+            version => Err(Error::InvalidVersion(version)),
+        }
     }
 }
 
@@ -133,13 +142,19 @@ fn decrypt_v3(crypto: &Crypto, password: &str) -> Result<Vec<u8>, Error> {
 
 #[cfg(test)]
 mod test {
+    use hex::FromHex;
+    use secp256k1;
     use super::*;
 
     #[test]
     fn open_account() {
         let buf = r#"{"address":"8125721c2413d99a33e351e1f6bb4e56b6b633fd","crypto":{"cipher":"aes-128-ctr","ciphertext":"ee00310ad9ab03f7b85cbabf72b919c7ed15f80b71f9531c167823995b28057a","cipherparams":{"iv":"67365b4d1ba21d457f3dbb22e46b627a"},"kdf":"scrypt","kdfparams":{"dklen":32,"n":4096,"p":6,"r":8,"salt":"0b28c47f7b5cf61be98e157c1f12d52e039120c28f42d50291f4674dc262ef8f"},"mac":"5f370bb0bcc879ed0db74716cbd24861ab0a9839e6985b82f9c2b48a127b6453"},"id":"c0137aac-b22c-42f0-9b42-192af24fe103","version":3}"#;
-        let account = Account::from_rd(buf.as_bytes(), "any").unwrap();
+        let account = Account::from_rd(buf.as_bytes()).unwrap();
 
         assert_eq!("c0137aac-b22c-42f0-9b42-192af24fe103", account.id());
+
+        let secret = account.decrypt("any").unwrap();
+        let x = secp256k1::Secp256k1::new();
+        assert_eq!(SecretKey::from_slice(&x, &"a5dd45e0810ca83e21f1063e6bf055bd13544398f280701cbfda1346bcf3ae64"[..].from_hex().unwrap()).unwrap(), secret);
     }
 }
